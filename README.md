3739b65156f1393d6e620f8846553e55868c7c36
Binnah <bnchoi.st@gamil.com>
2023-12-15 14:05:50 +0100

    Added data and r codes with first draft of reports

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # tb_prediction
+
+## Data collection
+Data provided by countries and territories are selected as predictor variables
+- Case notifications 
+- Drug resistance testing in pulmonary bacteriologically confirmed TB patients since 2017
+- Laboratory diagnostic services
+- Non-routine surveillance of HIV prevalence in TB patients
+- TB policies and services in 2022
+- Community engagement activities for TB
+- Budgets for TB since fiscal year 2018
+- Expenditure and utilization of health services for TB since fiscal year 2017
+- GDP per capita in 2022
+- The number of population in 2022
+
+() 14 rows are deleted
+() Missing imputation
+0-imputation for categorical variables
+Median imputation for continous variables
+## Description
+
+## How to run
+- Rmd file is in `rcodes` directory.
+
+## Further study
+
+## References
+[WHO TB data](https://www.who.int/teams/global-tuberculosis-programme/data)
+[World Bank](https://data.worldbank.org)
+### Copyright
+> Bich Na Choi
---
+++ rcodes/gtb_theme.R
@@ -0,0 +1,76 @@
+#' ggplot2 theme for the WHO Global TB Report
+#'
+#' @param base_size base font size, given in pts.
+#' @param base_family base font family
+#' @param axis_text_size font size for axis text in pts.
+#' @author: Hazim Timimi
+#' @examples
+#' p <- ggplot(mtcars, aes(wt, mpg)) +
+#'      geom_point() + facet_wrap(~ cyl)
+#' print(p)
+#' p2 <- p + theme_gtb()
+#' print(p2)
+#'
+#' @import ggplot2
+#' @export
+#'
+theme_gtb <-
+    function(base_size = 12,
+             base_family = "",
+             axis_text_size = 9) {
+        faint_gray <- "#CCCCCC"
+        gray <- "#BCBCBC"
+        charcoal <- "#222222"
+        
+        ggplot2::theme(
+            # Text format:
+            plot.margin = ggplot2::margin(30, 5, 30, 5),
+            
+            # Legend format
+            legend.position = "bottom",
+            legend.text.align = 0,
+            legend.background = ggplot2::element_blank(),
+            legend.title = ggplot2::element_blank(),
+            legend.key = ggplot2::element_blank(),
+            legend.text = ggplot2::element_text(
+                family = base_family,
+                size = base_size,
+                colour = charcoal
+            ),
+            
+            # Axis format
+            axis.title = ggplot2::element_text(
+                family = base_family,
+                size = base_size,
+                colour = charcoal
+            ),
+            
+            axis.text = ggplot2::element_text(
+                family = base_family,
+                size = axis_text_size,
+                colour = charcoal
+            ),
+            axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
+            axis.ticks.x = ggplot2::element_line(colour = gray),
+            axis.ticks.y = ggplot2::element_blank(),
+            axis.line.x = ggplot2::element_line(colour = gray, size = 0.25),
+            axis.line.y = ggplot2::element_blank(),
+            
+            # Grid lines
+            panel.grid.minor = ggplot2::element_blank(),
+            panel.grid.major.y = ggplot2::element_line(colour = faint_gray, size = 0.1),
+            panel.grid.major.x = ggplot2::element_blank(),
+            
+            # Have plain background in headings of facet sub-plots
+            strip.background = ggplot2::element_rect(fill = "white", colour = NA),
+            strip.text = ggplot2::element_text(
+                family = base_family,
+                size = base_size,
+                face = "bold",
+                colour = charcoal
+            ),
+            
+            # Blank background
+            panel.background = ggplot2::element_blank(),
+        )
+    }
